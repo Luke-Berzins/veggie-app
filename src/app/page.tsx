772aef7abda51173fcf8b994d91eb61d1bd988d0
Luke-Berzins <lukeberzins16@gmail.com>
2024-10-31 22:20:37 -0400

    new photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import Interactive from '@/clientComponents/Interactive'
-
-
+import Image from 'next/image';
+import Interactive from '@/clientComponents/Interactive';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center border-black-800 h-screen w-screen overflow-x-hidden">
-      <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight py-12 text-gray-900 md:text-6xl lg:text-6xl dark:text-white"><mark 
-            className="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Cowculator</mark></h1>
+      {/* Displaying the Image */}
+      <Image
+        src="/images/cow.png" // Path relative to the `public` folder
+        alt="Cow"
+        width={500} // Desired width in pixels
+        height={500} // Desired height in pixels
+      />
       <Interactive />
     </main>
-  )
+  );
 }
